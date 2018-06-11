bba94571871845707fdb4cea85b2d35dd422b46e
Matteo Hertel <info@matteohertel.com>
2018-06-11 21:38:23 +0100

    update build step

--- app/firebase.js
+++ app/firebase.js
@@ -1,8 +1,9 @@
+const firebaseConfig = require('firebaseConfig.json')
 module.exports = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  databaseURL: firebaseConfig.databaseURL,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId
 }
