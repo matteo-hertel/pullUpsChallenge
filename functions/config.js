8fbcd2f18b246d880e153f6714ac890c6b917904
Matteo Hertel <info@matteohertel.com>
2018-06-14 22:22:53 +0100

    update config

--- functions/config.js
+++ functions/config.js
@@ -13,18 +13,22 @@ const {
   upper_treshold
 } = functions.config().pulluptracking;
 
+const TRESHOLD_INCREASE = 0.5;
+const TOTAL_INCREASE = 2;
 const weekDifference = getDifferenceInWeeksToToday(start_date);
-const passedWeeks = weekDifference > 0 ? 0 : weekDifference;
+const passedWeeks = weekDifference > 0 ? weekDifference : 0;
 const int = i => parseInt(i, 10);
 
 module.exports = {
   endHour: int(end_hour),
   env,
-  lowerTreshold: int(lower_treshold) + passedWeeks,
+  lowerTreshold:
+    int(lower_treshold) + Math.floor(TRESHOLD_INCREASE * passedWeeks),
   passedWeeks,
   todoistAPIKey: token,
   todoistProject: todoist_project,
-  totalAmount: int(base_amount) * (passedWeeks || 1),
-  upperTreshold: int(upper_treshold) + passedWeeks,
+  totalAmount: int(base_amount) + TOTAL_INCREASE * (passedWeeks || 1),
+  upperTreshold:
+    int(upper_treshold) + Math.floor(TRESHOLD_INCREASE * passedWeeks),
   startHour: int(start_hour)
 };
