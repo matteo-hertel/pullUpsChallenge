a1d05e9ff24122da626e17021325474bc7cd6a43
Matteo Hertel <info@matteohertel.com>
2018-09-24 22:47:57 +0100

    update pullups creation logic

--- functions/libs/pullups.js
+++ functions/libs/pullups.js
@@ -1,17 +1,11 @@
 function createSet(total, upperTreshold, lowerTreshold) {
-  let remaining = total;
-  const sets = [];
-  while (remaining > 0) {
-    if (remaining <= lowerTreshold) {
-      sets.push(remaining);
-      break;
-    }
-    const set = Math.round(Math.random() * remaining);
-    if (set < lowerTreshold || set > upperTreshold) continue;
-    sets.push(set);
-    remaining -= set;
-  }
-  return sets;
+  return new Array(total).fill(lowerTreshold).map(luckySet(upperTreshold));
+}
+function luckySet(max) {
+  return function luckyDraw(n) {
+    if (Math.random() > 0.5) return max;
+    return n;
+  };
 }
 
-module.exports = { createSet };
+module.exports = {createSet};
--- functions/libs/pullups.test.js
+++ functions/libs/pullups.test.js
@@ -1,8 +1,8 @@
-const { createSet } = require("./pullups.js");
+const {createSet} = require('./pullups.js');
 
-describe("Pullups functions", () => {
-  test("createSet", () => {
-    const set = createSet(20, 8, 4);
+describe('Pullups functions', () => {
+  test('createSet', () => {
+    const set = createSet(4, 16, 12);
     expect(set.length).toBeDefined();
     expect(set.length).toBeGreaterThan(0);
   });
