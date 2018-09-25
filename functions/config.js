889023ceb04c32b23b300a1e995125bdcdda6bbc
Matteo Hertel <info@matteohertel.com>
2018-09-25 08:53:57 +0100

    fix old config logic

--- functions/config.js
+++ functions/config.js
@@ -1,5 +1,4 @@
-const functions = require("firebase-functions");
-const { getDifferenceInWeeksToToday } = require("./libs/date");
+const functions = require('firebase-functions');
 
 const {
   base_amount,
@@ -8,27 +7,20 @@ const {
   lower_treshold,
   start_date,
   start_hour,
-  todoist: { token },
+  todoist: {token},
   todoist_project,
-  upper_treshold
+  upper_treshold,
 } = functions.config().pulluptracking;
 
-const TRESHOLD_INCREASE = 0.5;
-const TOTAL_INCREASE = 2;
-const weekDifference = getDifferenceInWeeksToToday(start_date);
-const passedWeeks = weekDifference > 0 ? weekDifference : 0;
 const int = i => parseInt(i, 10);
 
 module.exports = {
   endHour: int(end_hour),
   env,
-  lowerTreshold:
-    int(lower_treshold) + Math.floor(TRESHOLD_INCREASE * passedWeeks),
-  passedWeeks,
+  lowerTreshold: lower_treshold,
   todoistAPIKey: token,
   todoistProject: todoist_project,
-  totalAmount: int(base_amount) + TOTAL_INCREASE * (passedWeeks || 1),
-  upperTreshold:
-    int(upper_treshold) + Math.floor(TRESHOLD_INCREASE * passedWeeks),
-  startHour: int(start_hour)
+  totalAmount: base_amount,
+  upperTreshold: upper_treshold,
+  startHour: int(start_hour),
 };
